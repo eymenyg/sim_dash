9b03121f5de6e9ec81ad9528d791d10cd506c80c
eymenyg <eymenyg01@gmail.com>
2026-05-08 21:13:26 -0400

    Arduino: update model dir structure and example code

--- Arduino/libraries/HyundaiDashV1/examples/HDV1_Generic/HDV1_Generic.ino
+++ Arduino/libraries/HyundaiDashV1/examples/HDV1_Generic/HDV1_Generic.ino
@@ -1,6 +1,11 @@
 #include <HyundaiDashV1.h>
 
-#include "Soul_PS.h"
+// Uncomment one of the following include blocks:
+
+// #include "models/Elantra_MD_11-13.h"
+// #include "models/Optima_TF_14-15.h"
+// #include "models/Sonata_YF_11-14.h"
+// #include "models/Soul_PS_14-16.h"
 
 HyundaiDashV1* activeCluster = &cluster;
 
